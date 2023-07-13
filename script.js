ee73afe4bc961739841ffa7eefe8d9f9f22ea814
JennieGalmin <Jennie.Galmin@gmail.com>
2023-07-13 19:16:54 +0200

    Set the location

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-// fetch url for 
+let lat = document.getElementById("lat");
+let long = document.getElementById("long");
+let alt = document.getElementById("alt");
+let vel = document.getElementById("vel");
+
+
+// fetch url for satiellite 25544 
 async function ISSLocation(){
     let response = await fetch("https://api.wheretheiss.at/v1/satellites/25544");
     let data = await response.json();
-
-
     console.log(data);
+
+lat.innerText = data.latitude;
+long.innerText = data.longitude;
+alt.innerText = data.altitude;
+vel.innerText = data.velocity;
 }
 
 ISSLocation();
+
+
 
