8588d7afc2561b9955887d00d33988f741835144
JennieGalmin <Jennie.Galmin@gmail.com>
2023-07-14 10:57:22 +0200

    Entered the map with marker

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+
 let lat = document.getElementById("lat");
 let long = document.getElementById("long");
 let alt = document.getElementById("alt");
 let vel = document.getElementById("vel");
+
 
 
 // fetch url for satiellite 25544 
@@ -10,6 +12,7 @@ async function ISSLocation(){
     let data = await response.json();
     console.log(data);
 
+//Getting the data to HTML
 lat.innerText = data.latitude;
 long.innerText = data.longitude;
 alt.innerText = data.altitude;
@@ -18,5 +21,29 @@ vel.innerText = data.velocity;
 
 ISSLocation();
 
+//API for google map
 
+let map;
 
+async function initMap(){
+let bettna = {lat: 58.540, lng:16.375};
+map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: bettna,
+   
+
+});
+
+let marker = new google.maps.Marker({
+    map:map,
+    position: bettna,
+    title: "bettna"
+});
+}
+/*let map = new google.maps.Map(
+    document.getElementById("map"), {zoom:  4, center:bettna}
+)
+let marker = new google.maps.Marker({position:bettna, map:map})
+};*/
+
+initMap()
